6e0c9e262ba179a20b26b4d5cfa2bc9d21325377
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-09-10 09:12:27 +0000

    Deploying to gh-pages from @ SheihMaks/goit-react-hw-08-phonebook@0cc9dccb37a2c2f83a0a5679a31f0bed4cf007aa 🚀

--- static/js/129.932cdc8d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[129],{2129:function(n,e,t){t.r(e),t.d(e,{NotFound:function(){return d}});var o,i,r=t(168),c=t(6031),u=c.ZP.div(o||(o=(0,r.Z)(["\npadding: 15% 0;\ntext-align:center;"]))),h=c.ZP.p(i||(i=(0,r.Z)(["\nfont-size:24px;\nfont-weight:600;\n"]))),a=t(184),d=function(){return(0,a.jsx)(u,{children:(0,a.jsx)(h,{children:"Page not found"})})}}}]);
-//# sourceMappingURL=129.932cdc8d.chunk.js.map
---
+++ static/js/129.dd8400d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[129],{2129:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var o,i,r=t(168),c=t(6031),u=c.ZP.div(o||(o=(0,r.Z)(["\npadding: 15% 0;\ntext-align:center;"]))),h=c.ZP.p(i||(i=(0,r.Z)(["\nfont-size:24px;\nfont-weight:600;\n"]))),a=t(184),f=function(){return(0,a.jsx)(u,{children:(0,a.jsx)(h,{children:"Page not found"})})}}}]);
+//# sourceMappingURL=129.dd8400d9.chunk.js.map
